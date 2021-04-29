ec5a28e0379f03c03cb00f8736896206ea6a6896
LEONARDO MAIA DE LIMA <02572655209@ssp.infopol.am.gov.br>
2021-04-29 17:04:50 -0300

    tratando a row

---
+++ android/app/src/main/kotlin/com/example/pontosvolei/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pontosvolei
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,192 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'App Placar de Volei',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Placar de Volei'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _time1 = 0;
+  int _time2 = 0;
+  double larguraBotao = 150;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Center(
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                //TIME 1
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      _time1++;
+                    });
+                  },
+                  child: Container(
+                    height: 150,
+                    width: larguraBotao,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.blue,
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          '$_time1',
+                          style: TextStyle(
+                            fontSize: 100,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                //TIME 2
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      _time2++;
+                    });
+                  },
+                  child: Container(
+                    height: 150,
+                    width: larguraBotao,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.red,
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          '$_time2',
+                          style: TextStyle(
+                            fontSize: 100,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          SizedBox(height: 30),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                    _time1--;
+                  });
+                },
+                child: Container(
+                  child: Center(
+                    child: Text(
+                      "-1",
+                      style: TextStyle(
+                        fontSize: 38,
+                      ),
+                    ),
+                  ),
+                  height: 50,
+                  width: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.blue,
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                    _time2--;
+                  });
+                },
+                child: Container(
+                    child: Center(
+                      child: Text(
+                        "-1",
+                        style: TextStyle(
+                          fontSize: 38,
+                        ),
+                      ),
+                    ),
+                    height: 50,
+                    width: 50,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.red,
+                    )),
+              ),
+            ],
+          ),
+          SizedBox(height: 30),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                    _time1 = 0;
+                    _time2 = 0;
+                  });
+                },
+                child: Container(
+                  height: 50,
+                  width: MediaQuery.of(context).size.width * 0.9,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.grey,
+                  ),
+                  child: Text(
+                    "ZERAR TUDO",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 40,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
